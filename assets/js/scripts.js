21205964b986b3c9d84bac34e3637f60f17c04cd
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-07-29 19:08:57 -0400

    avance de proyecto

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+
+
+
+document.querySelector('.header__button--register').addEventListener('click', function() {
+    window.open('register.html', '_blank'); // Abre register.html en una nueva ventana
+  });
+  
+
+  
