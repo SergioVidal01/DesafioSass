edbc91d328a8df920f41a20b55a83ccc52bfa587
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-07-29 20:14:14 -0400

    funcionalidades completas y paginas completas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,11 +5,13 @@ document.querySelector('.header__button--register').addEventListener('click', fu
     window.open('register.html', '_blank'); // Abre register.html en una nueva ventana
   });
   document.querySelector('.header__button--subscribe').addEventListener('click', function() {
-    window.open('subscription.html', '_blank'); // Abre register.html en una nueva ventana
+    window.open('subscription.html', '_blank'); // Abre subscription.html en una nueva ventana
   });
-  document.querySelector('.header__button--register').addEventListener('click', function() {
-    window.open('register.html', '_blank'); // Abre register.html en una nueva ventana
+  document.querySelector('.header__button--post1').addEventListener('click', function() {
+    window.open('post1.html', '_blank'); // Abre post1.html en una nueva ventana
   });
+
+  
   
 
   
