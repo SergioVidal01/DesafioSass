c5408707a2fb13332e006ab1202e25d8b81d480a
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-07-29 20:04:27 -0400

    add new pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,12 @@
 document.querySelector('.header__button--register').addEventListener('click', function() {
     window.open('register.html', '_blank'); // Abre register.html en una nueva ventana
   });
+  document.querySelector('.header__button--subscribe').addEventListener('click', function() {
+    window.open('subscription.html', '_blank'); // Abre register.html en una nueva ventana
+  });
+  document.querySelector('.header__button--register').addEventListener('click', function() {
+    window.open('register.html', '_blank'); // Abre register.html en una nueva ventana
+  });
   
 
   
